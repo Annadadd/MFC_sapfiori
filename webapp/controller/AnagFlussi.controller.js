aee726ab49ae8cfc44da5c5c6c0479a680cb22b8
Angelo Sisinna <a.sisinna@gmail.com>
2023-01-20 13:58:26 +0000

    pre-destroy

--- webapp/controller/AnagFlussi.controller.js
+++ webapp/controller/AnagFlussi.controller.js
@@ -10,10 +10,10 @@ sap.ui.define([
     "sap/ui/core/Core",
     "./Utils",
     "sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	"sap/ui/model/FilterType",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterType",
     "sap/ui/core/UIComponent"
-], function (ControlMessageProcessor, Message, Controller, coreLibrary, JSONModel, MessagePopover, MessagePopoverItem, MessageToast, oCore, Utils,Filter,FilterOperator,FilterType,UIComponent) {
+], function (ControlMessageProcessor, Message, Controller, coreLibrary, JSONModel, MessagePopover, MessagePopoverItem, MessageToast, oCore, Utils, Filter, FilterOperator, FilterType, UIComponent) {
     "use strict";
 
     var MessageType = coreLibrary.MessageType;
@@ -39,10 +39,8 @@ sap.ui.define([
                     processor: oMessageProcessor
                 })
             );
+
         },
-
-
-
 
         onPress: function (oEvent) {
 
@@ -95,40 +93,44 @@ sap.ui.define([
             return UIComponent.getRouterFor(this);
 
         },
-        add: function() {
+        add: function () {
             this.getRouter().navTo("RouteAddFlusso");
-          },
+        },
+
 
         refresh: function () {
-           
-                this.byId("nome").setValue(""),
+
+            this.byId("nome").setValue(""),
                 this.byId("descrizione").setValue(""),
                 this.byId("modulo").setValue("")
         },
         save: function () {
 
-                this.byId("nome").setProperty("editable", false),
+            this.byId("nome").setProperty("editable", false),
                 this.byId("descrizione").setProperty("editable", false),
                 this.byId("modulo").setProperty("editable", false),
-               
+
                 this.byId("refreshBtn").setProperty("visible", false),
                 this.byId("saveBtn").setProperty("visible", false),
                 this.byId("editBtn").setProperty("visible", true),
                 this.byId("tableUtentiFlussi").byId("eliminaUtenteDaFlussoBtn").setProperty("enabled", false)
         },
-         edit: function () {
+        // edit: function () {
 
-             this.byId("nome").setProperty("editable", true);
-             this.byId("descrizione").setProperty("editable", true);
-             this.byId("modulo").setProperty("editable", true);
-            
-                 this.byId("refreshBtn").setProperty("visible", true),
-                 this.byId("saveBtn").setProperty("visible", true),
-                 this.byId("editBtn").setProperty("visible", false),
-                 this.getView().byId("eliminaUtenteDaFlussoBtn").setProperty("enabled", true)
-    
-         },
-        
+
+        //     this.byId("nome").setProperty("editable", true);
+        //     this.byId("descrizione").setProperty("editable", true);
+        //     this.byId("modulo").setProperty("editable", true);
+
+        //     this.byId("refreshBtn").setProperty("visible", true),
+        //         this.byId("saveBtn").setProperty("visible", true),
+        //         this.byId("editBtn").setProperty("visible", false),
+        //         this.getView().byId("eliminaUtenteDaFlussoBtn").setProperty("enabled", true)
+        // },
+
+
+
+
         onSelectionChange: function (oEvent) {
             var oList = oEvent.getSource(),
                 bSelected = oEvent.getParameter("selected");
@@ -138,6 +140,17 @@ sap.ui.define([
                 // get the list item, either from the listItem parameter or from the event's source itself (will depend on the device-dependent mode).
                 this._showDetail(oEvent.getParameter("listItem") || oEvent.getSource());
             }
+
+            var utentiAssociati = this.byId("tableUtentiFlussi").getItems();
+            var uA = [];
+            uA.push(utentiAssociati);
+            var utenti = this.byId("tableUtenti").getItems();
+            var u = [];
+            u.push(utenti);
+
+            console.log(uA)
+             console.log(u)      
+            //
         },
         _onObjectMatched: function (oEvent) {
 
@@ -147,7 +160,6 @@ sap.ui.define([
         },
         _showDetail: function (oItem) {
             var id = oItem.getBindingContext("flussiModel").getProperty("id"); //prendo l'elemento da selezionare tramite id nella master page
-            console.log(id);
             this.getView().byId("detail").bindElement({ path: "/flussi/" + id, model: "flussiModel" });//lo sparo dritto nella detail page
         },
         onSearch: function (oEvt) {
@@ -155,7 +167,7 @@ sap.ui.define([
                 aFilter = [new Filter("id", FilterOperator.Contains, sQuery),
                 new Filter("nome", FilterOperator.Contains, sQuery),
                 new Filter("modulo", FilterOperator.Contains, sQuery),
-            ],
+                ],
                 oTable = this.byId("list"),
                 oBinding = oTable.getBinding("items"),
                 oFilter = null;
@@ -167,56 +179,109 @@ sap.ui.define([
             }
             oBinding.filter(oFilter);
         },
+
         onSearchUtentiAssociati: function (oEvt) {
+
             var sQuery = oEvt.getParameter("query"),
                 aFilter = [new Filter("id", FilterOperator.Contains, sQuery),
                 new Filter("cognome", FilterOperator.Contains, sQuery),
                 new Filter("nome", FilterOperator.Contains, sQuery),
-                
-            ],
+
+                ],
                 oTable = this.byId("tableUtentiFlussi"),
+                oTable1 = this.byId("tableUtenti"),
                 oBinding = oTable.getBinding("items"),
+                oBinding1 = oTable1.getBinding("items"),
                 oFilter = null;
             if (sQuery.length != 0) {
                 oFilter = new Filter({
                     filters: aFilter,
                     and: false
                 });
+
             }
+
             oBinding.filter(oFilter);
-        },
-        aggiungiUtenteAFlusso:function(){
-            this.byId("tableUtenti").setProperty("visible",true)
+            oBinding1.filter(oFilter);
         },
 
         
 
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        aggiungiUtenteAFlusso: function () {
+            this.byId("tableUtenti").setProperty("visible", true)
+        },
+
+
+        // edit: function (oEvent) {
+        //     var items = this.getView().byId("tableUtentiFlussi").getItems();
+
+        //     for (var i = 0; i < items.length; i++) {
+
+        //         var oTable = this.getView().byId("tableUtentiFlussi");
+        //         var nvalue = oTable.getItems()[i];
+
+        //         for (var y = 0; y < 4; y++) {
+        //             var celle = nvalue.getCells()[y];
+        //             if (y == 3) {
+        //                 celle.setProperty("enabled", true)
+        //             }
+        //         }
+        //     }
+        // },
+        
+        edit: function (oEvent) {
+            var items = this.getView().byId("tableUtentiFlussi").getItems();
+
+            for (var i = 0; i < items.length; i++) {
+
+                var oTable = this.getView().byId("tableUtentiFlussi");
+                var nvalue = oTable.getItems()[i];
+
+                for (var y = 0; y < 4; y++) {
+                    var celle = nvalue.getCells()[y];
+                    if (y == 3) {
+                        celle.setProperty("enabled", true)
+                    }
+                }
+            }
+        },
+
+
+
+
     });
-
-
     return PageController;
-
 });
-
-
-// edit: function (oEvent) {
-
-//     var id= this.getView().byId("eliminaUtenteDaFlussoBtn").getProperty("enabled", false);
-    
-
-//     if(id==false){
-//          id=this.setProperty("enabled", true)
-//          console.log(id);
-//     }
-   
-    
-    
-
-
-
-
-    
-
-// }
-
-
+ 
